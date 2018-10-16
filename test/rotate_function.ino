b29a42abb83665a6b9fa8092a87cdc9c6a0f9ff4
Rocco Matera <rmatera@purdue.edu>
2018-10-16 17:11:53 -0400

    Add files via upload

---
+++ test/rotate_function.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int rate = 100;
+  rotate(rate);
+}
+void rotate(int rate){
+  int pin;
+  for (pin=8;pin>=5;pin--){
+    digitalWrite(pin, HIGH);
+    delay(rate);
+    digitalWrite(pin, LOW);
+  }
+}
+
